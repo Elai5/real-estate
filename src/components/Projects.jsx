3c5aff8bdceec10cbc47ff0b3da897e83a3aa735
Elai5 <yvettelaine007@gmail.com>
2025-03-17 20:20:10 +0300

    text bot bottom, using margin for card slider

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,11 @@ const Projects = () => {
         <div className="flex gap-8 transition-transform duration-500 ease-in-out">
           {projectsData.map((project, index) => (
             <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
-              <img src={project.image} alt={project.title} />
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-auto mb-14"
+              />
               <div className="absolute left-0 right-0 bottom-5 flex justify-center">
                 <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
                   <h2 className="text-xl font-semibold text-gray-800">

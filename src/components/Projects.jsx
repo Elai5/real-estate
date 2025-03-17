5212de7c4d9b5d6495497fd9bfd402d63c405fe0
Elai5 <yvettelaine007@gmail.com>
2025-03-17 20:36:35 +0300

    added usestate for prev and next project using arrrowfunctions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,20 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState(1);
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+  };
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1
+    );
+  };
   return (
     <div
       className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
@@ -21,12 +32,14 @@ const Projects = () => {
       {/* slidderbutoons */}
       <div className="flex justify-end items-center mb-8">
         <button
+          onClick={prevProject}
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Previous-Project"
         >
           <img src={assets.left_arrow} alt="Previous" />
         </button>
         <button
+          onClick={nextProject}
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Next-Project"
         >

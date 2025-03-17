6ef2c29f8069f0291a4c137992e52439e704bb30
Elai5 <yvettelaine007@gmail.com>
2025-03-17 12:03:36 +0300

    styled about heading

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,15 @@ const About = () => {
       className="flex flex-col items-center justify-center container mx-auto md:px-20 lg:px-32 w-full overfflow-hidden"
       id="About"
     >
-      <h1>
-        About <span>Our Brand</span>
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+        About{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Our Brand
+        </span>
       </h1>
-      <p>Passionate about propperties, Dedicated to your vision</p>
+      <p className="text-gray-500 max-w-80 tect-center mb-8">
+        Passionate About Propperties, Dedicated to Your Vision
+      </p>
     </div>
   );
 };

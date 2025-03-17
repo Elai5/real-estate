116223615a8372eefc56f526543d248e4687efc9
Elai5 <yvettelaine007@gmail.com>
2025-03-17 11:59:03 +0300

    added about page component

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center container mx-auto md:px-20 lg:px-32 w-full overfflow-hidden"
+      id="About"
+    >
+      <h1>
+        About <span>Our Brand</span>
+      </h1>
+      <p>Passionate about propperties, Dedicated to your vision</p>
+    </div>
+  );
+};
+
+export default About;

1d910f80cb63ccc3587e9655e094dbdd39e33553
Elai5 <yvettelaine007@gmail.com>
2025-03-18 15:36:41 +0300

    added navbar on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,38 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="">
-      <div>
-        <div>
+    <div
+      className="pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden"
+      id="Footer"
+    >
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
           <img src={assets.logo_dark} alt="" />
-          <p>
+          <p className="text-gray-400 mt-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
             rem at facere.
           </p>
         </div>
-        <div></div>
+        <div className="w-full md:w-1/5 mb-8 md:mb-0">
+          <h3 className="text-white text-lg font-bold mb-4">Company</h3>
+          <ul className="flex flex-col gap-2 text-gray-400">
+            <a href="#Header" className="hover:text-white">
+              Home
+            </a>
+            <a href="#About" className="hover:text-white">
+              About Us
+            </a>
+            <a href="#Project" className="hover:text-white">
+              Project
+            </a>
+            <a href="#Contact" className="hover:text-white">
+              Contact Us
+            </a>
+            <a href="#" className="hover:text-white">
+              Privacy Policy
+            </a>
+          </ul>
+        </div>
         <div></div>
       </div>
       <div></div>

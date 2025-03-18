c3410ff60001cc01e8f0c3fbd99753b2b28d1c59
Elai5 <yvettelaine007@gmail.com>
2025-03-18 15:25:35 +0300

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="">
+      <div>
+        <div>
+          <img src={assets.logo_dark} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            rem at facere.
+          </p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;

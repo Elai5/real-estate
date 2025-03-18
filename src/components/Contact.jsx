c826e38949e8e840d9aae22c1acc581c203681ae
Elai5 <yvettelaine007@gmail.com>
2025-03-18 14:40:27 +0300

    added contect page,styled header

---
+++ src/components/Contact.jsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Contact{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Ready To Make A move? Lets Build Your Future Together
+      </p>
+    </div>
+  );
+};
+
+export default Contact;

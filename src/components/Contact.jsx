7b5a17fd2c542822831be60dd8329666111cbc28
Elai5 <yvettelaine007@gmail.com>
2025-03-18 15:03:37 +0300

    added messaging factor with web 3 and access tokens

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,33 @@
 import React from "react";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "80cf046d-5987-477d-98c6-56ed9f12b9a0");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      alert("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message);
+      setResult("");
+    }
+  };
+
   return (
     <div
       className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
@@ -18,7 +45,10 @@ const Contact = () => {
         Ready To Make A move? Lets Build Your Future Together
       </p>
 
-      <form className="max-w-2xl mx-auto text-gray-600 pt-8">
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+      >
         <div className="flex flex-wrap">
           <div className="w-full md:w-1/2 text-left">
             Your Name
@@ -51,8 +81,7 @@ const Contact = () => {
           ></textarea>
         </div>
         <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
-          {" "}
-          Send Message
+          {result ? result : "Send Message"}
         </button>
       </form>
     </div>
